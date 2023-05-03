1e2a91a53f1b1648c6d70866b265ce68e50904e1
shuGit1404 <132471593+shuGit1404@users.noreply.github.com>
2023-05-03 20:17:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-2c5wpa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-2c5wpa)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import Slider from '@mui/material/Slider';
+
+export default function App() {
+  return (
+    <>
+      <audio controls autoplay>
+        <source
+          src="https://www.zapsplat.com/wp-content/uploads/2015/sound-effects-61905/zapsplat_multimedia_alert_chime_short_musical_notification_cute_child_like_001_64918.mp3?_=1"
+          type="audio/mpeg"
+        />
+      </audio>
+    </>
+  );
+}
